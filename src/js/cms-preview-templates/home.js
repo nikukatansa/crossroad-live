946b84aad377c7f2571226ce2fcacc2ad6f59295
nikukatansa <nikukatansa@users.noreply.github.com>
2020-03-17 20:44:36 +0000

    Initial admin update - worship and sermon blocks

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -2,7 +2,6 @@ import React from "react";
 import format from "date-fns/format";
 
 import Jumbotron from "./components/jumbotron";
-import NoticeBlock from "./components/notice-block";
 
 export default class PostPreview extends React.Component {
   render() {
@@ -48,52 +47,54 @@ export default class PostPreview extends React.Component {
           </div>
         </div>
 
+        <div className="bg-off-white pv4">
+          <div className="ph3 mw7 center">
+            <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "worshipintro", "heading"])}</h2>
+            <p className="mb4 mw-100">{entry.getIn(["data", "worshipintro", "text"])}</p>
 
-
-
-
+            <div className="flex-ns">
+              {(entry.getIn(["data", "worshipvideos"]) || []).map((vid, i) => <div key={i} style={{width:'100%'}}>
+                <div style={{position: 'relative', paddingBottom: '56.25%', marginBottom: '30px', height: '0', overflow: 'hidden', maxWidth: '100%'}}>
+                  <iframe
+                  src={`//www.youtube.com/embed/${vid.video}`}
+                  style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}
+                  allowfullscreen
+                  frameborder="0"
+                  title={`${vid.title}`}
+                  ></iframe>
+                </div>
+              </div>)}
+            </div>
+          </div>
+        </div>
 
         <div className="bg-off-white pv4">
           <div className="ph3 mw7 center">
-            <h2 className="f2 b lh-title mb2">
-              {entry.getIn(["data", "intro", "heading"])}
-            </h2>
-            <p className="mb4 mw6">{entry.getIn(["data", "intro", "text"])}</p>
-
-            <div className="flex-ns mhn2-ns mb3">
-              {(entry.getIn(["data", "products"]) || []).map((product, i) => <div className="ph2-ns w-50-ns" key={i}>
-                <img src={getAsset(product.get("image"))} alt="" className="center db mb3" style={{ width: "240px" }}/>
-                <p>{product.get("text")}</p>
-              </div>)}
-            </div>
-
-            <div className="tc">
-              <a href="#" className="btn raise">See all products</a>
+            <h2 className="f2 b lh-title mb2">This week's message</h2>
+            <p className="mb4 mw6">{entry.getIn(["data", "sermon", "title"])}</p>
+            <div className="flex-ns">
+              <div style={{width:'100%'}}>
+                <div style={{position: 'relative', paddingBottom: '56.25%', marginBottom: '30px', height: '0', overflow: 'hidden', maxWidth: '100%'}}>
+                  <iframe
+                    src={`//www.youtube.com/embed/${entry.getIn(["data", "sermon", "video"])}`}
+                    style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}
+                    allowfullscreen
+                    frameborder="0"
+                    title={`${entry.getIn(["data", "sermon", "title"])}`}
+                  ></iframe>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="bg-grey-1 pv4">
-          <div className="ph3 mw7 center">
-            <div className="flex-l mhn2-l">
-              <div className="w-40-l ph2-l">
-                <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "values", "heading"])}</h2>
-
-                <p>{entry.getIn(["data", "values", "text"])}</p>
-              </div>
-
-              <div className="w-60-l ph2-l">
-                <img src="/img/home-about-section.jpg" alt="" className="mb3"/>
-              </div>
-            </div>
-
-            <div className="tc">
-              <a href="{{.buttonLink}}" className="btn raise">
-                Read more
-              </a>
-            </div>
+          <div className="flex-l mhn1-l ph3 center mw7">
+            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blessing", "heading"])}</h2>
+            <p className="w-60-l mb0">{entry.getIn(["data", "blessing", "text"])}</p>
           </div>
         </div>
+
       </div>
   }
 }
