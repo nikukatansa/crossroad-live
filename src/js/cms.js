0fa5eda713f5b843bf40d57a27bb4dd87de304f1
nikukatansa <nikukatansa@users.noreply.github.com>
2020-05-07 21:07:56 +0100

    Test of initial prayer page embedding

--- src/js/cms.js
+++ src/js/cms.js
@@ -6,10 +6,10 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import ContactPreview from "./cms-preview-templates/contact";
+import ContactPreview from "./cms-preview-templates/prayer";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
+CMS.registerPreviewTemplate("prayer", ContactPreview);
 CMS.init();
